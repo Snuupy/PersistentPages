1887015b0b0c87c0b9548c7055d70097ab6c5e7c
Snuupy <brian.elists@gmail.com>
2017-06-24 17:19:57 -0400

    full screen

--- main.js
+++ main.js
@@ -1,56 +1,52 @@
 var winRef; //This holds the reference to your page, to see later it is open or not
 
 function openWindow() {
-    var url = "popup.html" //Your URL;
-    if (typeof (winRef) == 'undefined' || winRef.closed) {
+    var url = "popup.html"; //Your URL;
+    if (typeof winRef == "undefined" || winRef.closed) {
         //create new, since none is open
         console.log("opening window because none exists");
         winRef = window.open(
-        	"popup.html",
-        	"Popup",
-        	"directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no"
-        	);
-        winRef.moveTo(500, 100);
+            "popup.html",
+            "Popup",
+            "directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no"
+        );
+        // winRef.moveTo(500, 100);
     } else {
-    	try {
-    		winRef.document;
-    	}
-    	catch (e) {
-    		winRef = window.open(url, "_blank");
-    	}
-    	if (navigator.appName == 'Microsoft Internet Explorer') {
-    		winRef.close();
-    		winRef = window.open(url, "_blank");
-        	//handling for IE
+        try {
+            winRef.document;
+        } catch (e) {
+            winRef = window.open(url, "_blank");
         }
-        else {
-        	winRef.focus();
-        	console.log("window already open, refocusing");
+        if (navigator.appName == "Microsoft Internet Explorer") {
+            winRef.close();
+            winRef = window.open(url, "_blank");
+            //handling for IE
+        } else {
+            winRef.focus();
+            winRef.alert("hello");
+            console.log("window already open, refocusing");
         }
     }
+
 }
 
 window.onbeforeunload = function() {
-	return "Are you sure you want to navigate away?";
-}
-
+    return "Are you sure you want to navigate away?";
+};
 
 window.onclick = () => {
- 	openWindow();
+    openWindow();
 };
 
 window.onkeydown = function(e) {
-	var key = e.keyCode ? e.keyCode : e.which;
-	//tab = 9
-	//ctrl = 17
-	//alt = 18
-	if (e.keyCode == 17 || e.keyCode == 9 || e.keyCode == 18) {
-		openWindow();
-		alert("test");
-	}
+    var key = e.keyCode ? e.keyCode : e.which;
+    //tab = 9
+    //ctrl = 17
+    //alt = 18
+    if (e.keyCode == 17 || e.keyCode == 9 || e.keyCode == 18) {
+        openWindow();
+    }
 };
-
-
 
 // document.getElementById("open").click();
 // console.log("tried to open page");
@@ -63,3 +59,10 @@ window.onkeydown = function(e) {
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API
 // http://www.thefutureoftheweb.com/blog/detect-browser-window-focus
+
+$(window)
+    .focus(function() {
+        console.log("Focus changed");
+        winRef.alert();
+    })
+    .blur(function() {});
